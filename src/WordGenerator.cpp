63d156e749a516cf005e215ff7b9817dfc8922a3
Biyu <biyuwts@gmail.com>
2026-06-04 21:22:38 +0700

    generate kata baru dari word_module

--- src/WordGenerator.cpp
+++ src/WordGenerator.cpp
@@ -1,40 +1,16 @@
 #include "WordGenerator.h"
-#include <vector>
+#include "word_module.h"
 #include <random>
 
-// menggunakna namspace anonim agar fungsi di file ini tidak bisa dipakai di luar file
 namespace {
-    // Word lists
-    const std::vector<std::string> easy = {
-        "cat", "dog", "sun", "tree", "book",
-        "run", "fish", "bird", "moon", "star",
-        "ship", "mars", "earth", "venus", "rock"
-    };
-
-    const std::vector<std::string> medium = {
-        "planet", "rocket", "system", "typing",
-        "comet", "orbit", "alien", "space",
-        "laser", "pilot", "galaxy", "meteor",
-        "probe", "saturn"
-    };
-
-    const std::vector<std::string> hard = {
-        "asteroid", "galaxy", "algorithm", "development",
-        "supernova", "astronaut", "telescope", "constellation",
-        "spaceship", "gravity", "universe", "starlight",
-        "satellite", "blackhole"
-    };
-
-    // Random engine (dibuat sekali)
     std::mt19937 rng(std::random_device{}());
 
     const std::string& pick(const std::vector<std::string>& words) {
-        std::uniform_int_distribution<> dist(0, words.size() - 1);
+        std::uniform_int_distribution<> dist(0, static_cast<int>(words.size()) - 1);
         return words[dist(rng)];
     }
 }
 
-// fungsi untuk mengambil kata secara acak dengan tingkat kesulitan yang dipilih
 const std::string& WordSystem::getRandomWord(Difficulty diff) {
     switch (diff) {
         case Difficulty::EASY:
